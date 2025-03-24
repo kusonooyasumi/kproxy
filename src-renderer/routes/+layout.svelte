250ef130a28590d5c605d952882a0a3197530478
oyasumi <kusonooyasumi@protonmail.com>
2025-03-24 14:01:25 -0500

    linux compat changes

--- src-renderer/routes/+layout.svelte
+++ src-renderer/routes/+layout.svelte
@@ -10,13 +10,13 @@
 	});
 </script>
 
-<div id='titlebar' class='shrink-0 bg-gradient-to-r from-gray-100 to-gray-200 flex text-slate-950 dark:from-[#2a2a2a] dark:to-[#2a2a2a] dark:text-slate-50'>
+<div id='titlebar' class='shrink-0 flex'>
 	<div class='px-4 select-none grow text-xs flex items-center' style='-webkit-app-region: drag;'>Kproxy</div>
 	{#if import.meta.env.DEV}
 		<Devtools/>
 	{/if}
 </div>
-<div class='overflow-auto h-full bg-gradient-to-br from-white to-zinc-50 text-slate-950 dark:from-zinc-800 dark:to-zinc-900 dark:text-slate-50'>
+<div class='overflow-auto h-full'>
 	{@render children()}
 </div>
 
